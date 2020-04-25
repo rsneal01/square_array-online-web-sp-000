77e57002ef5425685a72215c2d75796d8b660e08
Bobby Neal <rsneal01@gmail.com>
2020-04-25 16:18:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
   squared_array = []
-  array.each { |number| squared_array << number ** 2 }
+  array.each do { |number| squared_array << number ** 2 }
   squared_array
 end
