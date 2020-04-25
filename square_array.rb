d53a231e239a16eb75bae7a8aa14afce2b2912f1
Bobby Neal <rsneal01@gmail.com>
2020-04-25 16:21:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
+# def square_array(array)
+#   squared_array = []
+#   array.each { |number| squared_array << number ** 2 }
+#   squared_array
+# end
+
+
 def square_array(array)
   squared_array = []
   array.each { |number| squared_array << number ** 2 }
   squared_array
 end
-
